be5349f760777b56ba41b9a6f186220d7b7bde7a
Daniil <dkurpachenko@gmail.com>
2019-09-16 17:25:55 +0300

    Updates

---
+++ precache-manifest.011e381965b7ba172502dc867b84d658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/yoi3-0/miniapp2.git/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "cd28470a22178bbc9b1d",
+    "url": "/yoi3-0/miniapp2.git/static/js/runtime~main.4d490665.js"
+  },
+  {
+    "revision": "a66fc844f045d0b5be28",
+    "url": "/yoi3-0/miniapp2.git/static/js/main.e4276d17.chunk.js"
+  },
+  {
+    "revision": "a11579b5dd97dde499ba",
+    "url": "/yoi3-0/miniapp2.git/static/js/2.3627d7be.chunk.js"
+  },
+  {
+    "revision": "a66fc844f045d0b5be28",
+    "url": "/yoi3-0/miniapp2.git/static/css/main.b8384eb1.chunk.css"
+  },
+  {
+    "revision": "a11579b5dd97dde499ba",
+    "url": "/yoi3-0/miniapp2.git/static/css/2.8b86948f.chunk.css"
+  },
+  {
+    "revision": "3e649afa8dae41890fe087a82ab7fb45",
+    "url": "/yoi3-0/miniapp2.git/index.html"
+  }
+];
