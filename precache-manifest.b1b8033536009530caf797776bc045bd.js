d15b6eb27a030eb471d9326de3219a676806d6fc
Daniil <dkurpachenko@gmail.com>
2019-09-17 20:41:31 +0300

    Updates

---
+++ precache-manifest.b1b8033536009530caf797776bc045bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "98189222df7c7279494c",
+    "url": "./static/js/main.0d292f92.chunk.js"
+  },
+  {
+    "revision": "8fe086370ac637492b3a",
+    "url": "./static/js/2.8bf106e3.chunk.js"
+  },
+  {
+    "revision": "8fe086370ac637492b3a",
+    "url": "./static/css/2.8b86948f.chunk.css"
+  },
+  {
+    "revision": "5a72f5c937a77a161dcc101a788d41b5",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d12c5b04e46dda05220815975113417b.js"
+  "./precache-manifest.b1b8033536009530caf797776bc045bd.js"
 );
 
 workbox.clientsClaim();
