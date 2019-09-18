78bd9cbbac62daafd94dbaa6524bb11bed15bc68
Daniil <dkurpachenko@gmail.com>
2019-09-18 15:04:49 +0300

    Design big update

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
-import { HeaderButton, TabbarItem, View, Tabbar, Epic, platform, IOS} from '@vkontakte/vkui';
+import { Panel, ListItem,  Tooltip, Cell, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
+import { HeaderButton, Search, Switch, List, TabbarItem, View, Tabbar, Epic, platform, IOS} from '@vkontakte/vkui';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Icon28Messages from '@vkontakte/icons/dist/28/messages';
@@ -9,6 +9,8 @@ import Icon28Search from '@vkontakte/icons/dist/28/search';
 import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
 import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
 import Icon28More from '@vkontakte/icons/dist/28/more';
+import Icon28SettingsOutline from '@vkontakte/icons/dist/28/settings_outline';
+import Icon28FavoriteOutline from '@vkontakte/icons/dist/28/favorite_outline';
 
 const osname = platform();
 
@@ -17,7 +19,7 @@ class Example extends React.Component {
 	  super(props);
   
 	  this.state = {
-		activeStory: 'more'
+		activeStory: 'activity'
 	  };
 	  this.onStoryChange = this.onStoryChange.bind(this);
 	}
@@ -33,62 +35,66 @@ class Example extends React.Component {
 		  <Tabbar>
 			<TabbarItem
 			  onClick={this.onStoryChange}
-			  selected={this.state.activeStory === 'feed'}
-			  data-story="feed"
-			  text="Новости"
+			  selected={this.state.activeStory === 'activity'}
+			  data-story="activity"
+			  text="Тренажёры"
 			><Icon28Newsfeed /></TabbarItem>
 			<TabbarItem
 			  onClick={this.onStoryChange}
-			  selected={this.state.activeStory === 'discover'}
-			  data-story="discover"
-			  text="Поиск"
-			><Icon28Search /></TabbarItem>
-			<TabbarItem
-			  onClick={this.onStoryChange}
-			  selected={this.state.activeStory === 'messages'}
-			  data-story="messages"
+			  selected={this.state.activeStory === 'usermap'}
+			  data-story="usermap"
 			  label="1"
-			  text="Сообщения"
-			><Icon28Messages /></TabbarItem>
+			  text="Прогресс"
+			><Icon28FavoriteOutline /></TabbarItem>
 			<TabbarItem
 			  onClick={this.onStoryChange}
-			  selected={this.state.activeStory === 'notifications'}
-			  data-story="notifications"
-			  text="Уведомлен."
-			><Icon28Notifications /></TabbarItem>
-			<TabbarItem
-			  onClick={this.onStoryChange}
-			  selected={this.state.activeStory === 'more'}
-			  data-story="more"
-			  text="Ещё"
-			><Icon28More /></TabbarItem>
+			  selected={this.state.activeStory === 'settings'}
+			  data-story="settings"
+			  text="Настройки"
+			><Icon28SettingsOutline /></TabbarItem>
 		  </Tabbar>
 		}>
-		  <View id="feed" activePanel="feed">
-			<Panel id="feed">
-			  <PanelHeader>Feed</PanelHeader>
-			</Panel>
-		  </View>
-		  <View id="discover" activePanel="discover">
-			<Panel id="discover">
-			  <PanelHeader>Discover</PanelHeader>
-			</Panel>
-		  </View>
-		  <View id="messages" activePanel="messages">
-          <Panel id="messages">
+		  <View id="activity" activePanel="activity">
+          <Panel id="activity">
 		  <PanelHeader>
-			  Кек
+			  Тренажёры
 			</PanelHeader> 
+			<Search />
+			<Group title="Тренажёры" description="Выберете интересующий Вас тренажёр.">
+        <List>
+          <Cell expandable onClick={() => this.setState({ activePanel: 'nothing' })}  description="Поможет Вашим глазам раслабиться">Глазотрен-3000</Cell>
+          <Cell expandable onClick={() => this.setState({ activePanel: 'nothing' })} description="Приведёт в чувства конечности">Рукоблуд форте</Cell>
+          <Cell expandable onClick={() => this.setState({ activePanel: 'nothing' })} description="Повысит Вашу концентрацию">Самогонщик онлайн</Cell>
+        </List>
+      </Group>
 			</Panel>
 		  </View>
-		  <View id="notifications" activePanel="notifications">
-			<Panel id="notifications">
-			  <PanelHeader>Notifications</PanelHeader>
+		  <View id="usermap" activePanel="usermap">
+			<Panel id="usermap">
+			 <PanelHeader>Карта достижений</PanelHeader>
 			</Panel>
 		  </View>
-		  <View id="more" activePanel="more">
-			<Panel id="more">
-			  <PanelHeader>More</PanelHeader>
+		  <View id="settings" activePanel="settings">
+			<Panel id="settings">
+			  <PanelHeader>Настройки</PanelHeader>
+			   <Group>
+				  <div  align='center'>
+			  <Avatar size={100}   />
+			     <Cell
+                  size="l"
+                  description="Тренируется с 18.09.2019"
+                >
+                  Имя Фамилия</Cell> 
+				  </div>
+			 </Group> 
+
+			<Group>
+              <List>
+                <Cell asideContent={<Switch />}>Уведомления</Cell>
+                <Cell expandable onClick={() => this.setState({ activePanel: 'nothing' })}>Выбор темы</Cell>
+                <Cell expandable onClick={() => this.setState({ activePanel: 'nothing' })}>Сообщество сервиса</Cell>
+              </List>
+            </Group>
 			</Panel>
 		  </View>
 		</Epic>
